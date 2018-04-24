3ba71784b05baaf2315ec2af39807b94460542b6
Mateusz Bąkała <bakala12@o2.pl>
2018-04-24 17:31:57 +0200

    Adding command parser file

---
+++ app/CommandParser.hs
@@ -0,0 +1,15 @@
+module CommandParser 
+    where
+
+--hit init
+--hit diff <file>
+--hit status
+--hit commit <message>
+--hit checkout <branchName>
+--hit checkout <commitHash>
+--hit newbranch <branchName>
+--hit removebranch <branchName>
+--hit merge <branchName>
+--hit log
+--hit history <int>
+
