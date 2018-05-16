bf4d0735ca93bbd36cd47267399064599e231e58
Mateusz Bąkała <bakala12@o2.pl>
2018-05-16 15:54:42 +0200

    Fixing find in tree

--- src/Hit/Snapshot/Directory.hs
+++ src/Hit/Snapshot/Directory.hs
@@ -56,9 +56,18 @@ getCommitFromHash hash = getPathToObject hash >>= restoreCommit
 
 findInTreeHelper :: [FilePath] -> Hash -> ExIO Hash
 findInTreeHelper [] hash = return hash
-findInTreeHelper (x:xs) hash = getTreeFromHash hash >>= return . (findFirstMatching (\b a -> ((entryName a)++"/")==b) x) . entries >>= (\r -> (lift $ putStrLn $ show r) >> return r) >>= (\r -> case r of
-    Nothing -> throwE ("Cannot find file "++x)
-    (Just e) -> findInTreeHelper xs (entryHash e)) 
-
+findInTreeHelper (x:xs) hash = do{
+    --lift $ putStrLn x;
+    tree <- getTreeFromHash hash;
+    --lift $ putStrLn ("Hash: "++hash);
+    --lift $ putStrLn ("Found: "++(hashObject tree));
+    ent <- return $ entries tree;
+    m <- return $ findFirstMatching (\b a -> (((entryName a)++"/")==b) || (entryName a)==b) x ent;
+    --lift $ putStrLn $ show $ map (\e -> ((entryName e)++" "++(entryHash e))) ent;
+    --lift $ putStrLn ("Matching: "++ (show m));
+    case m of
+        Nothing -> throwE ("Cannot find "++x)
+        (Just x) -> findInTreeHelper xs (entryHash x)
+}
 findInTree :: FilePath -> Tree -> ExIO Blob
 findInTree path tree = getRepositoryDirectory >>= (\p -> return $ splitAndGetRest p path) >>= (\l -> findInTreeHelper l (hashObject tree)) >>= getPathToObject >>= restoreBlob
