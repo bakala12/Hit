014b12200b170bd38b58608bf70554c2f535baa4
Mateusz Bąkała <bakala12@o2.pl>
2018-05-28 11:36:24 +0200

    Reset changes command fully works

--- src/Hit/Snapshot/Reset.hs
+++ src/Hit/Snapshot/Reset.hs
@@ -3,9 +3,22 @@ module Hit.Snapshot.Reset where
 import Hit.Common.Data
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.Class
+import Hit.Repository.References
+import Hit.Snapshot.Directory
+import Hit.Common.File
+import Hit.Repository
+import Hit.Snapshot.Checkout
+import Hit.Objects
+import Control.Applicative
 
 resetChangesInFile :: FilePath -> ExIO ()
-resetChangesInFile path = return ()
+resetChangesInFile path = do{
+    baseTree <- getCurrentBranchVersion;
+    baseVersion <- catchE (Just <$> findFileInTree path baseTree) (\e -> return Nothing);
+    case baseVersion of
+        Nothing -> catchE (removeExistingFile path) (\e -> return ())
+        (Just x) -> writeWholeFile path (fileContent x)
+}
 
 resetAllChanges :: ExIO ()
-resetAllChanges = return ()
+resetAllChanges = getCurrentBranch >>= makeBranchCheckout
