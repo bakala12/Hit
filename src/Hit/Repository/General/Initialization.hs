5230825d3d12706de148ad586dda977793daf841
Mateusz Bąkała <bakala12@o2.pl>
2018-06-01 22:21:56 +0200

    Making hit init command multithreaded

--- src/Hit/Repository/General/Initialization.hs
+++ src/Hit/Repository/General/Initialization.hs
@@ -1,23 +1,36 @@
 -- A module that exposes functions connected with Hit repository
-module Hit.Repository.General.Initialization where
+module Hit.Repository.General.Initialization (
+    initRepository,
+    isInitialized
+)where
 
 import Hit.Common.Data
 import Hit.Common.File
 import Hit.Common.Repository
 import Control.Monad.Trans.Except
+import Control.Monad.Trans.Class
+import Control.Concurrent.Async
+import Control.Applicative
+import System.IO
+import System.Directory
+import Data.Foldable (traverse_)
 
 -- | Initializes a new empty Hit repository in the current directory
 initRepository :: ExIO ()
 initRepository = do{
     path <- getHitDirectoryPath;
     createEmptyDirectory path;
-    createNewFile path ".hitconfig" "username=User\nemail=email@example.com";
-    createNewFile path ".hitlog" "hit init";
-    createNewFile path "head" "refs/master";
-    createEmptyDirectory (path++"/objects");
-    createEmptyDirectory (path++"/refs");
-    createNewFile path "refs/master" ""
-} 
+    secureFileOperation $ runOperations path
+}
+
+operations :: FilePath -> [(IO ())]
+operations path = [(writeFile (path++"/.hitconfig") "username=User\nemail=email@example.com"),
+    (writeFile (path++"/head") "refs/master"),
+    (createDirectory (path++"/objects")),
+    ((createDirectory (path++"/refs") >> (writeFile (path++"/refs/master") "")))]
+
+runOperations :: FilePath -> IO ()
+runOperations path = runConcurrently $ traverse_ Concurrently $ operations path
 
 -- | Checks if the repository is initialized.
 isInitialized :: ExIO Bool
